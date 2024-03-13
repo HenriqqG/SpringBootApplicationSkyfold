0d9d7510abb4b3a3a88874cb30b553d965268a7b
Henrique Gagno Porto <91996657+HenriqqG@users.noreply.github.com>
2024-03-13 14:24:23 -0300

    initial commit

---
+++ src/main/java/com/org/springbootskyfold/SpringbootskyfoldApplication.java
@@ -0,0 +1,13 @@
+package com.org.springbootskyfold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootskyfoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootskyfoldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/org/springbootskyfold/SpringbootskyfoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.springbootskyfold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootskyfoldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
